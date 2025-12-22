46aef49dba916bb43c6066aa250210428874efd5
Neto test <akochetkov1312@gmail.com>
2025-12-22 19:36:46 +0300

    Commit change three

--- src/game/Game.js
+++ src/game/Game.js
@@ -233,8 +233,7 @@ export default class Game {
       finalScoreElement.textContent = this.score.getScore();
       console.log(`Final score: ${this.score.getScore()}`);
     }
-
-    // Show game over modal
+    
     const gameOverModal = document.getElementById('game-over-modal');
     if (gameOverModal) {
       gameOverModal.classList.add('active');
